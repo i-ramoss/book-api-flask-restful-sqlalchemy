54ee0e10a1ec16dec3124c633c571de660dce173
Ian Ramos <ianramossantos@hotmail.com>
2022-07-09 13:41:46 -0300

    chore: instantiate marshmallow serializer

---
+++ ma.py
@@ -0,0 +1,6 @@
+from flask_marshmallow import Marshmallow
+
+# the marshmallow is responsible to serialize the data and return on python object or json format in request
+
+# instantiate the marshmallow class
+ma = Marshmallow()
