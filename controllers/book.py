5bb60105177c4c3953c4c0fcb03461c4bfd522f3
Ian Ramos <ianramossantos@hotmail.com>
2022-07-09 13:45:08 -0300

    feat: book controllers to receive app requests

---
+++ controllers/book.py
@@ -0,0 +1,21 @@
+from flask import request
+from flask_restplus import Resource, fields
+
+from models.book import BookModel
+from schemas.book import BookSchema
+
+from server.instance import server
+
+book_ns = server.book_ns
+
+book_schema = BookSchema()
+book_list_schema = BookSchema(many=True)
+
+
+class Book(Resource):
+    def get(self, id):
+        book_data = BookModel.find_by_id(id)
+
+        if book_data:
+            print("Book Data directly from database: ", book_data)
+            return book_schema.dump(book_data)
