9571001ebc4871840f4af3ed592a05d9ada16c09
Ian Ramos <ianramossantos@hotmail.com>
2022-07-09 14:10:15 -0300

    feat: route to create a book

--- controllers/book_controller.py
+++ controllers/book_controller.py
@@ -17,7 +17,25 @@ class Book(Resource):
         book_data = BookModel.find_by_id(id)
 
         if book_data:
-            print("Book Data directly from database: ", book_data)
             return book_schema.dump(book_data), 200
 
         return {"message": "Book not found"}, 404
+
+
+class BookList(Resource):
+    def get(self):
+        return book_list_schema.dump(BookModel.find_all()), 200
+
+    @book_ns.expect(item)
+    @book_ns.doc("Create a book")
+    def post(self):
+        # o metodo get_json() pega todos os dados que estão no body da requisição
+        book_json = request.get_json()
+
+        # recebe todo o dicionário python e o transforma em um objeto que pode
+        # ser utilizado no banco de dados
+        book_data = book_schema.load(book_json)
+
+        book_data.save_to_db()
+
+        return book_schema.dump(book_data), 201
